941b40f67df683ebe9482e23d9b7d992b848f292
Mathieu Bouchonnet <mathieubouchonnet@gmail.com>
2025-08-24 15:37:47 +0200

    Add fugitive/copilot support

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -53,4 +53,11 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 vim.keymap.set('n', '<leader>pv', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
 
+-- copilot
+vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+  expr = true,
+  replace_keycodes = false,
+})
+vim.g.copilot_no_tab_map = true
+
 -- vim: ts=2 sts=2 sw=2 et
