c291189f6d57868c9dd5b2257a080bd2e3e98c4b
Mathieu Bouchonnet <mathieu.bouchonnet@ivory-tower.fr>
2025-09-23 09:58:16 +0200

    Change the gitsigns for add/change lines

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -18,8 +18,10 @@ return {
     'lewis6991/gitsigns.nvim',
     opts = {
       signs = {
-        add = { text = '+' },
-        change = { text = '~' },
+        --       add = { text = '+' },
+        --        change = { text = '~' },
+        add = { text = '┃' },
+        change = { text = '┃' },
         delete = { text = '_' },
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
