90442373e29742b8b31e03da094e38b585dd1914
mbouchonnet <mathieubouchonnet@gmail.com>
2025-11-01 20:25:31 +0100

    add support for clang cpp

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -96,6 +96,7 @@ return {
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
           map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+          map('<leader>i', '<CMD>ClangdSwitchSourceHeader<CR>', '[G]oto [D]eclaration')
 
           -- Fuzzy find all the symbols in your current document.
           --  Symbols are things like variables, functions, types, etc.
