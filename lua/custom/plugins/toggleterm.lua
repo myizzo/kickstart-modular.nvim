41d5ee69b76f9332b455f8390ebd01356000c98d
mbouchonnet <mathieubouchonnet@gmail.com>
2025-11-03 15:56:20 +0100

    add toggleterm

---
+++ lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,49 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  opts = {--[[ things you want to change go here]]
+    open_mapping = [[c-\]],
+  },
+  config = function()
+    require('toggleterm').setup()
+
+    vim.api.nvim_set_keymap('n', '<leader>0', '<cmd>:ToggleTerm size=20 name=main<CR>', { noremap = true, silent = true })
+
+    local Terminal = require('toggleterm.terminal').Terminal
+    local lazygit = Terminal:new {
+      cmd = 'lazygit',
+      dir = 'git_dir',
+      direction = 'float',
+      float_opts = {
+        border = 'double',
+      },
+      -- function to run on opening the terminal
+      on_open = function(term)
+        vim.cmd 'startinsert!'
+        vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
+      end,
+      -- function to run on closing the terminal
+      on_close = function(term)
+        vim.cmd 'startinsert!'
+      end,
+    }
+
+    function _lazygit_toggle()
+      lazygit:toggle()
+    end
+
+    vim.api.nvim_set_keymap('n', '<leader>g', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true })
+
+    function _G.set_terminal_keymaps()
+      local opts = { buffer = 0 }
+      vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+      vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+      vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+      vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+      vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+      vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+    end
+
+    vim.cmd 'autocmd! TermOpen term://* lua set_terminal_keymaps()'
+  end,
+}
